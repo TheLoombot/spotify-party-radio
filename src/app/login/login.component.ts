d7eccf8b6dc1e5a74324849b9e02aa8622fc4668
TheLoombot <4617505+TheLoombot@users.noreply.github.com>
2018-07-10 22:58:42 -0400

    First pass at "login with Spotify" button

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  client_id = "7fafbce74b0b4d78868fbdc6d6b1858b";
+  response_type = "token";
+  redirect_uri = "https://poop-50473.firebaseapp.com/redirect";
+  authorizationUrl = "https://accounts.spotify.com/authorize?"; 
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.authorizationUrl += "&client_id=" + this.client_id; 
+    this.authorizationUrl += "&response_type=" + this.response_type;
+    this.authorizationUrl += "&redirect_uri=" + this.redirect_uri;
+    this.authorizationUrl += "&scope=streaming";
+
+  }
+
+  authSpotify() {
+    window.open(this.authorizationUrl);
+  }
+
+}

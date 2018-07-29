eca31bc26b383dc45bec4d34667494ea39166040
TheLoombot <4617505+TheLoombot@users.noreply.github.com>
2018-07-29 01:25:22 -0400

    Cleanups in now playing component
    Some error handling, accounting for nothing playing, making sure there's a delay on first load to account for time for auth to complete

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -11,6 +11,7 @@ import { switchMap, map, startWith } from 'rxjs/operators';
 export class PlayerComponent implements OnInit {
 
     nowPlaying : Object;
+    playerError;
 
     constructor(private spotify: SpotifyService) {
 
@@ -19,14 +20,21 @@ export class PlayerComponent implements OnInit {
         // from Spotify when the track changes. The alternative approach
         // is to set a timer based on expiration time and just change tracks
         // then, instead of this way where we observe the playback state
-        interval(2000).pipe(startWith(0))
+        interval(2000)
         .pipe(switchMap(() => this.spotify.getNowPlaying()))
         .subscribe(
             (data) => {
                 this.nowPlaying = data
                 console.log("now playing ", this.nowPlaying)
-            })
-        }
+
+            }, 
+            error => {
+                this.playerError = error.error.error
+                console.log("player error ", this.playerError)
+            }
+
+            )
+    }
 
     ngOnInit() {
     }

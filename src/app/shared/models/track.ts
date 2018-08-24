ac1f9e33b5f99d172d6445f4f63a68086c3bb0af
Pablovem <pablovema@gmail.com>
2018-08-23 20:25:09 -0500

    feat: add track model

---
+++ src/app/shared/models/track.ts
@@ -0,0 +1,32 @@
+export class Track {
+  added_at: number;
+  added_by: string;
+  album?: Array<any>;
+  album_name: string;
+  album_url: string;
+  artists?: Array<any>;
+  artist_name: string;
+  available_markets?: Array<string>;
+  disc_number?: number;
+  duration_ms: number;
+  expires_at?: number;
+  explicit?: boolean;
+  external_ids?: {
+    isrc: string
+  };
+  external_urls?: {
+    spotify: string
+  };
+  href?: string;
+  id: string;
+  image_url: string;
+  is_local?: boolean;
+  name: string;
+  popularity?: number;
+  preview_url?: string;
+  track_number?: number;
+  type?: string;
+  uri: string;
+}
+
+

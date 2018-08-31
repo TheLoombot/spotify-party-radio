bffb324c548c6b139bdbc5d04bad1d11ab13f16f
Pablovem <pablovema@gmail.com>
2018-08-30 21:20:15 -0500

    fix: update search component with image validation

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,43 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
 import { Subject } from 'rxjs';
-import { PlaylistService } from '../playlist.service'
+import { PlaylistService } from '../playlist.service';
 
 @Component({
-	selector: 'app-search',
-	templateUrl: './search.component.html',
-	styleUrls: ['./search.component.css'],
-	providers: [SpotifyService]
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  providers: [SpotifyService]
 })
 export class SearchComponent implements OnInit {
+  results: Object;
+  searchTerm$ = new Subject<string>();
+  searchError;
+  clicked: number;
 
-	results: Object;
-	searchTerm$ = new Subject<string>();
-	searchError;
-	clicked : number;
+  constructor(private spotify: SpotifyService, private playlistSvc: PlaylistService) {
 
-	constructor(private spotify: SpotifyService, private playlistSvc: PlaylistService) { 
+    this.spotify.search(this.searchTerm$)
+    .subscribe(
+      results => {
+        this.results = results;
+        this.clicked = -1;
+        console.log('Results:', this.results);
+      },
+      error => {
+        this.searchError = error.error.error;
+      }
+    );
+  }
 
-		this.spotify.search(this.searchTerm$)
-		.subscribe(data => {
-			this.results = data
-			this.clicked = -1;
-			// console.log(this.results)
-		},
-		error => {
-			this.searchError = error.error.error
-		}
-		);
+  ngOnInit() {
+  }
 
-	}
-
-	ngOnInit() {
-	}
-
-	pushTrack(track: Object, i: number) {
-		this.clicked = i
-		this.playlistSvc.pushTrack(track)
-	}
-
-
+  pushTrack(track: Object, i: number) {
+    this.clicked = i;
+    this.playlistSvc.pushTrack(track);
+  }
 }

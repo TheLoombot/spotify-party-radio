62eaac14a12c3abc2f067d70ae3454d19a3795e0
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-06-06 10:45:28 -0400

    Update to Bootstrap 4.5.0 from 3.4.1 to get ngbCarousel working... first pass at super janky carousel

--- src/app/playerpicker/playerpicker.component.ts
+++ src/app/playerpicker/playerpicker.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { StateService } from '../shared/services/state.service';
+import { PlaylistService } from '../shared/services/playlist.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -9,9 +12,47 @@ import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/n
 })
 export class PlayerpickerComponent implements OnInit {
 
-  constructor() { }
+  currentStation: string;
+  stationSub: Subscription;
+  stations;
 
-  ngOnInit(): void {
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  constructor(
+    private stateService: StateService,
+    private playlistService: PlaylistService,
+    ) { }
+
+  ngOnInit() {
+    this.currentStation = this.playlistService.getStation();
+    this.stationSub = this.playlistService.getAllStations()
+    .subscribe(
+      stations => {
+        this.stations = stations;
+        console.log(`Total stations: ${this.stations['length']}`);
+        // console.log(this.stations);
+      },
+      error => console.error('Playlist retrieves error: ', error)
+      );
+    this.carousel.pause();
   }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    console.log(`slide events, yo`);
+    this.stateService.sendState({ enabled: true, loading: true, station: slideEvent.current });
+    this.playlistService.setStation(slideEvent.current);
+    setTimeout(() => {
+      this.stateService.sendState({ enabled: true, loading: false, station: slideEvent.current });
+    }, 1);
+    this.currentStation = slideEvent.current;
+  }
+
+
+  ngOnDestroy() {
+    if (this.stationSub) {
+      this.stationSub.unsubscribe();
+    }
+  }
+
 
 }

aa4f4ded3264ed81a91b37cdd488678ed5de609e
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-06-08 19:27:28 -0400

    Al/stationpicker (#66)
    * Format the station picker per spec in #58
    
    * worst night of my fucking life
    
    * picker updates
    
    * real shitty way to indicate "live" stations #61
    
    * new playerpicker component and adding ng-bootstrap to get its carousel module
    
    * Update to Bootstrap 4.5.0 from 3.4.1 to get ngbCarousel working... first pass at super janky carousel
    
    * Tidy up the grid layout a bit
    
    * Fix up login component for bootstrap 4
    
    * app some vertical margin to columns
    
    * fixing all the layout in the search module for bootstrap 4
    
    * Cleanup "Up Next" component for bootstrap fuckin 4
    
    * replace glyphicons with fontawesome 4.7.0 hosted by bootstrap
    
    * fixing search component to use fontawesome icons instead of glyphicons from bootstrap 3
    
    * more bootstrap-related fixes
    
    * semi-functional player-picker layout
    
    * killin the old player and more formatting cleanups for the new player-picker
    
    * doing things in threes instead of fives?
    
    * Make player responsive to station changes! fuckin finally

--- src/app/stationpicker/stationpicker.component.ts
+++ src/app/stationpicker/stationpicker.component.ts
@@ -12,7 +12,7 @@ export class StationpickerComponent implements OnInit {
 
   currentStation: string;
   stationSub: Subscription;
-  stations: Object;
+  stations;
 
   constructor(
     private stateService: StateService,
@@ -21,16 +21,36 @@ export class StationpickerComponent implements OnInit {
 
   ngOnInit() {
     this.currentStation = this.playlistService.getStation();
-    this.stationSub = this.playlistService.getStations()
+    this.stationSub = this.playlistService.getAllStations()
     .subscribe(
       stations => {
         this.stations = stations;
         console.log(`Total stations: ${this.stations['length']}`);
+        // console.log(this.stations);
       },
       error => console.error('Playlist retrieves error: ', error)
       );
   }
 
+  isLive(stationKey: string) {    
+    
+    //I don't understand why this doesn't work: 😭    
+    // console.log(this.stations[stationKey]);
+    // Because it doesn't, we have to loop through ALL stations
+
+    for (let station of this.stations) {
+      if (station.key == stationKey) {
+        if (station.lists.playlist) {
+          for (let track of Object.values(station.lists.playlist)) {
+            if (!track['player']) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
 
   setStation(stationKey) {
     this.stateService.sendState({ enabled: true, loading: true, station: stationKey });

b3e3f2b5431a63200ca57f96509e1f46478dab47
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-06-08 00:11:51 -0400

    semi-functional player-picker layout

--- src/app/playerpicker/playerpicker.component.ts
+++ src/app/playerpicker/playerpicker.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
-import { StateService } from '../shared/services/state.service';
+import { SpotifyService } from '../shared/services/spotify.service';
 import { PlaylistService } from '../shared/services/playlist.service';
-import { Subscription } from 'rxjs';
-
+import { StateService } from '../shared/services/state.service';
+import { interval, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { Track } from '../shared/models/track';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-playerpicker',
@@ -15,16 +18,52 @@ export class PlayerpickerComponent implements OnInit {
   currentStation: string;
   stationSub: Subscription;
   stations;
+  playlistSub: Subscription;  
+  firstTrackKey;
+  firstTrack;
+  progress: number;
+  showSkip: boolean;
+  showNowPlaying: boolean;
+  progressSub: Subscription;
+  timeOutSubs = [];
+  nowPlaying;
+  playerError;
 
   @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
 
   constructor(
-    private stateService: StateService,
+    private titleService: Title,
+    private spotifyService: SpotifyService,
     private playlistService: PlaylistService,
-    ) { }
+    private stateService: StateService
+    ) {     
+    this.progress = 0;
+    this.showSkip = false;
+    this.showNowPlaying = false;
+  }
 
   ngOnInit() {
     this.currentStation = this.playlistService.getStation();
+
+    this.playlistSub = this.playlistService.getFirstTracks(1).snapshotChanges()
+    .pipe(debounceTime(300))
+    .subscribe(
+      data => {
+        // console.log('Player data:', data);
+        if (data[0]) {
+          this.firstTrackKey = data[0].key;
+          this.firstTrack = data[0].payload.val();
+          // console.log(`First track ${this.firstTrackKey} is: ${this.firstTrack.name}`);
+          this.checkFirstTrack();
+        } else {
+          this.playlistService.autoUpdatePlaylist(); // handle empty playlist
+        }
+      },
+      error => {
+        console.log('playlist retrieve error:', error);
+      }
+      );
+
     this.stationSub = this.playlistService.getAllStations()
     .subscribe(
       stations => {
@@ -34,7 +73,32 @@ export class PlayerpickerComponent implements OnInit {
       },
       error => console.error('Playlist retrieves error: ', error)
       );
-    this.carousel.pause();
+
+    this.progressSub = interval(1000)
+    .subscribe(
+      () => {
+        if (this.firstTrack) {
+          this.progress = this.calcProgress(this.firstTrack); // Update Progress
+        }
+      }
+      );
+  }
+
+  skipTrack(key: string): void {
+    console.log(`Skipping and removing ${this.firstTrack.name} from pool `);
+    this.playlistService.removeFromPool(this.firstTrack.id);
+    this.spotifyService.pauseTrack()
+    .subscribe(
+      () => {},
+      error => {
+        console.error('Failed to pause track ', error);
+      }
+      );
+    this.timeOutSubs.forEach(id => clearTimeout(id));
+    this.showNowPlaying = false;
+    this.playlistService.remove(key, 0);
+    this.showSkip = false;
+    this.firstTrack = null;
   }
 
   onSlide(slideEvent: NgbSlideEvent) {
@@ -47,10 +111,123 @@ export class PlayerpickerComponent implements OnInit {
     this.currentStation = slideEvent.current;
   }
 
+  private setTitle(title: string) {
+    this.titleService.setTitle(title);
+  }
+
+  private calcProgress(firstTrack: Track): number {
+    return Math.floor( 100 * ( 1 - (firstTrack.expires_at - this.getTime() ) / firstTrack.duration_ms) );
+  }
+
+  private getTime(): number {
+    return new Date().getTime();
+  }
+
+  /**
+    Called either when the 'Now Playing' state changes, or when
+    when the first entry of the playlist changes.
+
+    Job is to:
+    * Check if first track is expired, if so delete it, ya done
+    * If not, check if it's already playing, if so ya done
+    * If it's not playing, play it, ya done
+    **/
+    checkFirstTrack() {
+      if (this.firstTrack == null) {
+        console.warn(`State of Brad: Sorry mate theres nothing to play`);
+        return;
+      }
+
+      // console.log(`Checking first track: ${this.firstTrack.name}`);
+      const timeToExpiration = this.getTime() - this.firstTrack.expires_at;
+      // console.log('First track expires at: ', this.showDate(this.firstTrack.expires_at));
+      // console.log('Time to first track expiration: ', timeToExpiration);
+
+      if (timeToExpiration > 0) {
+        // Track has expired
+        console.log(`${this.firstTrack.name} expired`);
+        // console.log(this.showDate(this.getTime()), 'expected expiration time was', this.showDate(this.firstTrack.expires_at));
+        this.playlistService.remove(this.firstTrackKey, 0);
+        this.playlistService.saveTrack(this.firstTrack); // Save track in secondary list
+        this.showSkip = false;
+        this.showNowPlaying = false;
+        this.firstTrack = null;
+        return;
+      }
+
+      this.spotifyService.getNowPlaying()
+      .subscribe(
+        (data: any) => {
+          this.nowPlaying = data ? data.item : null;
+          if (this.nowPlaying == null) {
+            console.warn('There is nothing being played');
+            this.timeOutSubs.forEach(id => clearTimeout(id));
+            this.timeOutSubs.push(
+              setTimeout( () => {
+                this.checkFirstTrack();
+              }, 2500)
+              );
+          } else if (this.firstTrack == null) {
+            console.warn('Sorry mate theres nothing to play');
+          } else if (data.is_playing && this.nowPlaying.uri === this.firstTrack.uri) {
+            console.log(`${this.nowPlaying.name} expires in ${timeToExpiration}`);
+            this.showNowPlaying = true;
+            this.setTitle(`${this.firstTrack.artist_name} - ${this.firstTrack.name}`);
+            this.showSkip = true;
+
+            this.timeOutSubs.forEach(id => clearTimeout(id));
+            this.timeOutSubs.push(
+              setTimeout( () => {
+                this.checkFirstTrack();
+              }, -timeToExpiration)
+              );
+
+          } else {
+            this.spotifyService.playTrack(this.firstTrack.uri)
+            .subscribe(
+              (response) => {
+                // this.playerError = response
+                console.log(`${this.firstTrack.name} requested playback, scheduled check in 1000ms`);
+                setTimeout(() => {
+                  this.checkFirstTrack();
+                }, 1000);
+                // playTrack doesn't give us an affirmative response on the play request
+                // so we have to wait a bit (fudge = 1.0s) before we try check the 'now
+                // playing status' again
+                this.spotifyService.seekTrack(this.getTime() - this.firstTrack.expires_at + this.firstTrack.duration_ms)
+                .subscribe(
+                  () => {},
+                  error => {
+                    console.error('Brads error: failed on seek', error);
+                  }
+                  );
+              },
+              error => {
+                this.playerError = error.error.error;
+                console.log('play back error ', this.playerError);
+              }
+              );
+          }
+        },
+        error => {
+          this.playerError = error.error.error;
+          console.error('Now playing error:', error);
+          this.stateService.sendError(`There is no available user, ${error.error.error.message}`, error.error.error.status);
+          this.setTitle('Logged out');
+        }
+        );
+    }
+
 
   ngOnDestroy() {
     if (this.stationSub) {
       this.stationSub.unsubscribe();
+    }
+    if (this.playlistSub) {
+      this.playlistSub.unsubscribe();
+    }
+    if (this.progressSub) {
+      this.progressSub.unsubscribe();
     }
   }
 

29a3aa7b8d5032c13191ec706d07bd09b5206dab
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-06-10 20:39:50 -0400

    Basic pagination of user's playlists

--- src/app/user-playlists/user-playlists.component.ts
+++ src/app/user-playlists/user-playlists.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../shared/services/spotify.service';
 import { PlaylistService } from '../shared/services/playlist.service';
+import { Subject } from 'rxjs';
 
 @Component({
 	selector: 'app-user-playlists',
@@ -9,33 +10,54 @@ import { PlaylistService } from '../shared/services/playlist.service';
 })
 export class UserPlaylistsComponent implements OnInit {
 
-  userPlaylistsEnabled: boolean;
-  userPlaylists; 
-  userPlaylistsError;
-  
+	userPlaylistsEnabled: boolean;
+	userPlaylists; 
+	userPlaylistsError;
+	showPlaylists: boolean;
+	showTracks: boolean;
+	playlistOffset$ = new Subject<number>();
+	curPlaylistOffset = 0;
+	curTracksOffset = 0;
+	pageSize = 3;
+
 	constructor(
 		private spotifyService: SpotifyService,
 		private playlistSvc: PlaylistService
 		) 
 	{ 
 		this.userPlaylistsEnabled = this.spotifyService.isTokenAvailable();
+		this.showPlaylists = true;
+		this.showTracks = false;
 	}
 
 	ngOnInit(): void {
 
+		this.playlistOffset$.next(this.curPlaylistOffset);
+
 		if (this.userPlaylistsEnabled) {
-		  this.spotifyService.getUserPlaylists()
-		    .subscribe(
-		      userPlaylists => {
-		        this.userPlaylists = userPlaylists;
-		      },
-		      error => {
-		        this.userPlaylistsError = error.error.error;
-		      }
-		    );
+			this.spotifyService.getUserPlaylists(this.playlistOffset$)
+			.subscribe(
+				userPlaylists => {
+					this.userPlaylists = userPlaylists;
+					// console.log(this.userPlaylists);
+				},
+				error => {
+					this.userPlaylistsError = error.error.error;
+				}
+				);
 		}
 
+		this.playlistOffset$.next(this.curPlaylistOffset);
+	}
 
+	nextPlaylistPage() {
+		this.curPlaylistOffset += this.pageSize;
+		this.playlistOffset$.next(this.curPlaylistOffset);
+	}
+
+	prevPlaylistPage() {
+		this.curPlaylistOffset -= this.pageSize;
+		this.playlistOffset$.next(this.curPlaylistOffset);
 	}
 
 	clickedPlaylist(playlist: Object) {

7531e21428ed428f0904c39a31a4c184a58893f2
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-07-21 14:14:30 -0400

    Restrict who can remove tracks from upcoming playlist... make this operation also delete track from the pool

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
 /* Services */
 import { PlaylistService } from '../shared/services/playlist.service';
+import { SpotifyService } from '../shared/services/spotify.service';
+import { Track } from '../shared/models/track';
 
 @Component({
   selector: 'app-playlist',
@@ -14,7 +16,8 @@ export class PlaylistComponent implements OnInit, OnDestroy {
   trackSub;
 
   constructor(
-    private playlistService: PlaylistService
+    private playlistService: PlaylistService,
+    private spotifyService: SpotifyService,
     ) {
   }
 
@@ -38,13 +41,28 @@ export class PlaylistComponent implements OnInit, OnDestroy {
       );
   }
 
+  userOwnsStation(): boolean {
+    if (this.currentStation === this.spotifyService.getUserName()) return true;
+    return false;
+  }
+
+  // You can remove tracks if it's your station, or if you just added the track
+  // to someone else's station... but not if it came out of the pool via Robot DJ
+  canRemoveTrack(track: Track): boolean {
+    if (this.userOwnsStation()) return true;
+    if (track.player) return false;
+    if (track.added_by === this.spotifyService.getUserName()) return true;
+    return false;
+  }
+
   removeTrack(track: any, i: number): void {
     // add 1 to the index because we slice off the first track
     i++;
     console.log(`Clicked to remove ${track.name}`);
     this.playlistService.remove(track.key, i);
     if (track.player) {
-      this.playlistService.saveTrack(track);
+      // this.playlistService.saveTrack(track);
+      this.playlistService.removeFromPool(track.id);
     }
   }
 

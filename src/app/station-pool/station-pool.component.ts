b9b36c0136131b8f120ef49784c485bde251c5d0
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-09-07 13:40:22 -0400

    new empty station pool component

---
+++ src/app/station-pool/station-pool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-station-pool',
+  templateUrl: './station-pool.component.html',
+  styleUrls: ['./station-pool.component.css']
+})
+export class StationPoolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

41fdbd6d719e1b9dcd85d24d49118c1987bed4b2
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-09-07 14:30:07 -0400

    basic display of pool tracks in component

--- src/app/station-pool/station-pool.component.ts
+++ src/app/station-pool/station-pool.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PlaylistService } from '../shared/services/playlist.service';
 
 @Component({
   selector: 'app-station-pool',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StationPoolComponent implements OnInit {
 
-  constructor() { }
+  @Input() currentStation: string;
+  tracks: Object;
+  trackSub;
+
+  constructor(
+  	private playlistService: PlaylistService,
+  	) { }
 
   ngOnInit(): void {
+    // ngOnChanges() is called on init! so nothing to do here
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.tuneToStation(changes['currentStation'].currentValue);
+  }
+
+  tuneToStation(stationName: string) {
+    this.trackSub?.unsubscribe();
+    this.trackSub = this.playlistService.getAllPreviousTracks()
+    .subscribe(
+      tracks => {
+        this.tracks = tracks;
+        console.log(`Pool update, new size: ${this.tracks['length']}`);
+      },
+      error => console.error('Playlist retrieves error: ', error)
+      );
   }
 
 }

a2990831b9b511774e861cc06812c4fdbc93048a
Aman Loomba <4617505+TheLoombot@users.noreply.github.com>
2020-09-13 17:30:29 -0400

    cleanup usage of appEnabled state

--- src/app/single-player-station/single-player-station.component.ts
+++ src/app/single-player-station/single-player-station.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { StateService } from '../shared/services/state.service';
 import { SpotifyService } from '../shared/services/spotify.service';
 import { PlaylistService } from '../shared/services/playlist.service';
-import { State } from '../shared/models/state';
 
 @Component({
   selector: 'app-single-player-station',
@@ -13,12 +11,10 @@ import { State } from '../shared/models/state';
 export class SinglePlayerStationComponent implements OnInit {
 
   station: string;
-  appEnabled: boolean;
 
   constructor(
     private cdr: ChangeDetectorRef,
     private route: ActivatedRoute,
-    private stateService: StateService,
     private spotifyService: SpotifyService,
     private playlistService: PlaylistService,
     ) 
@@ -26,30 +22,12 @@ export class SinglePlayerStationComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
-    this.stateService.getState()
-      .subscribe(
-        (state: State) => {
-          this.appEnabled = state.enabled;
-        },
-        error => console.error(error),
-        () => {
-          this.cdr.detectChanges();
-        }
-      );
-
       this.route.params.subscribe( 
         params => { 
-          if (!this.spotifyService.getToken()) {
-            this.stateService.sendError(`There is no available token?`);
-            return;
-          }
           this.station = params['station'] ;
           this.playlistService.setStation(this.station);
-          this.appEnabled = true;
         }
         );
-
   }
 
   userOwnsStation(): boolean {
